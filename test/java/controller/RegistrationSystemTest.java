2257ee98697e71be73f2f510bb47b99b67b6c43e
ohcommon4real <demian.iarina@gmail.com>
2021-11-01 01:39:10 +0200

    tests generated

---
+++ test/java/controller/RegistrationSystemTest.java
@@ -0,0 +1,32 @@
+package controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationSystemTest {
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void retriveCoursesWithFreePlaces() {
+    }
+
+    @Test
+    void retrieveStudentsEnrolledForACourse() {
+    }
+
+    @Test
+    void getAllCourses() {
+    }
+
+    @Test
+    void deleteCourse() {
+    }
+
+    @Test
+    void updateCreditsCourse() {
+    }
+}

d97d994a5c126782e66ebcb32d845842b5c417b8
ohcommon4real <demian.iarina@gmail.com>
2021-11-18 13:36:56 +0200

    all documentation done

--- src/main/java/controller/NonexistentArgumentException.java
+++ src/main/java/controller/NonexistentArgumentException.java
@@ -1,5 +1,8 @@
 package controller;
 
+/**
+ * Represents an exception triggered when the given argument does not exist in the repo
+ */
 public class NonexistentArgumentException extends RuntimeException{
     public NonexistentArgumentException(String message) {
         super(message);

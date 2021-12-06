700fce79dfe57b123a2a742e35384630c7e9fb28
ohcommon4real <demian.iarina@gmail.com>
2021-12-06 23:07:19 +0200

    course test working

--- test/java/model/CourseTest.java
+++ test/java/model/CourseTest.java
@@ -1,58 +1,58 @@
-//package model;
-//
-//import controller.AlreadyExistingException;
-//import controller.FullCourseException;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class CourseTest {
-//    private Student student;
-//    private Student student2;
-//    private Student student3;
-//    private Course course;
-//
-//
-//    @BeforeEach
-//    void init() {
-//        Teacher t1 = new Teacher("Ana", "Mare");
-//        course = new Course("c1", t1, 2, 10);
-//        t1.addCourse(course);
-//
-//        student = new Student("Mihai", "Dinero", 101);
-//        student2 = new Student("Matei", "Rus", 102);
-//        student3 = new Student("Mihaela", "Pop", 103);
-//
-//    }
-//
-//    @Test
-//    void addStudent() {
-//        course.addStudent(student);
-//        course.addStudent(student2);
-//        List<Student> expectedStudents = new ArrayList<>(Arrays.asList(student,student2));
-//        assertEquals(expectedStudents, course.getStudentsEnrolled());
-//
-//        //adding a already existing student
-//        try{
-//            course.addStudent(student);
-//        }
-//        catch (AlreadyExistingException e){
-//            assertEquals(expectedStudents, course.getStudentsEnrolled());
-//        }
-//
-//        //the maxEnrollement has been reached
-//        try{
-//            course.addStudent(student3);
-//        }
-//        catch (FullCourseException e){
-//            assertEquals(expectedStudents, course.getStudentsEnrolled());
-//            assertEquals(2,course.getStudentsEnrolled().size());
-//        }
-//
-//    }
-//}
+package model;
+
+import controller.AlreadyExistingException;
+import controller.FullCourseException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseTest {
+    private Student student;
+    private Student student2;
+    private Student student3;
+    private Course course;
+
+
+    @BeforeEach
+    void init() {
+        Teacher t1 = new Teacher(1,"Ana", "Mare");
+        course = new Course(1,"c1", 1, 2, 10);
+        t1.addCourse(course.getId());
+
+        student = new Student(1,"Mihai", "Dinero", 101);
+        student2 = new Student(2,"Matei", "Rus", 102);
+        student3 = new Student(3,"Mihaela", "Pop", 103);
+
+    }
+
+    @Test
+    void addStudent() {
+        course.addStudent(student.getId());
+        course.addStudent(student2.getId());
+        List<Integer> expectedStudents = new ArrayList<>(Arrays.asList(student.getId(),student2.getId()));
+        assertEquals(expectedStudents, course.getStudentsEnrolledId());
+
+        //adding a already existing student
+        try{
+            course.addStudent(student.getId());
+        }
+        catch (AlreadyExistingException e){
+            assertEquals(expectedStudents, course.getStudentsEnrolledId());
+        }
+
+        //the maxEnrollement has been reached
+        try{
+            course.addStudent(student3.getId());
+        }
+        catch (FullCourseException e){
+            assertEquals(expectedStudents, course.getStudentsEnrolledId());
+            assertEquals(2,course.getStudentsEnrolledId().size());
+        }
+
+    }
+}
